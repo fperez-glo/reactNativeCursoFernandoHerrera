3125570477970fc7c166766a3ed215410b0b5f0b
fperez-glo <facundoperez6@gmail.com>
2021-07-14 13:30:13 -0300

    Carga incial del repositorio

---
+++ src/typescript/TiposBasicos.tsx
@@ -0,0 +1,21 @@
+
+export const TiposBasicos = () => {
+
+    const nombre: string = 'Fernando';
+    const edad: number = 35;
+    const estaActivo : boolean = true;
+
+    const poderes : string[] = ['velocidad','volar','respirar bajo el agua']
+
+    return (
+        <>
+            <h3>Tipos Basicos</h3> 
+            {nombre}, {edad},{ (estaActivo) ? 'activo' : 'no activo'}
+            <br />
+            {poderes.join(', ')}
+            <br/>
+        </>
+    )
+}
+
+
